1315d30faf620645632df53abb75a5dd488e7ea1
Rajeev Mehra <130817555+RAJMEH753@users.noreply.github.com>
2024-05-06 22:21:33 +0530

    Update QuickSearch.js

--- Client/src/Pages/QuickSearch.js
+++ Client/src/Pages/QuickSearch.js
@@ -1,15 +1,16 @@
 import React from "react";
 /*import navHook from "./nav";*/
+import { Link } from "react-router-dom";
 
 class QuickSearch extends React.Component{
 
    /* showFilter = (ss) => {
         this.props.navigate(`/filter?mealtype=${ss}`);
-    } */
+    } 
 
      showFilter = (ss) => {
         this.props.history.push(`/filter?mealtype=${ss}`);
-    }
+    }*/
 
     render(){
         const { mealtypeData } = this.props;
@@ -35,7 +36,9 @@ class QuickSearch extends React.Component{
                         {
                             mealtypeData?.map((meal) => {
                                 return(
-                                    <div class="d-flex box mt-4" style={{border: "1px solid greenyellow;"}} onClick={() => this.showFilter(meal._id)}>
+                                   <Link to=`/filter?mealtype=${meal._id}`>
+                                    /* <div class="d-flex box mt-4" style={{border: "1px solid greenyellow;"}} onClick={() => this.showFilter(meal._id)}> */
+                                      <div class="d-flex box mt-4" style={{border: "1px solid greenyellow;"}} >
                                         <div class="l-box">
                                             <img src={`./images/${meal.image}`} alt="images_mealtype" class="img-fluid img-qs-m" />
                                         </div>
@@ -43,7 +46,7 @@ class QuickSearch extends React.Component{
                                             <h4 class="card-title">{meal.name}</h4>
                                             <p class="card-content">{meal.content}</p>
                                         </div>
-                                    </div>
+                                    </div> </Link>
                                 )
                             })
                         }
